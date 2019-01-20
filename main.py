2b03d0a7e1cc5ebb6a5853b3ce559a850bf9ab30
Thomas Bailey <baileytb7@gmail.com>
2019-01-20 00:09:47 -0500

    Best parameters can persist via log file

--- main.py
+++ main.py
@@ -1,79 +1,78 @@
 import gym
 import mujoco_py
 import torch
+from numpy import array
 import numpy as np
+import logging
+import datetime
 
-env = gym.make('HumanoidStandup-v2')
+np.set_printoptions(threshold=np.nan)
 
+
+# initialize
+model = 'HumanoidStandup-v2'
+env = gym.make(model)
+
+# useful variables
 action_size = len(env.action_space.high)
 observation_size = len(env.observation_space.high)
 
-print("action space: ", env.action_space)
-print("high: ", env.action_space.high)
-print("low: ", env.action_space.low)
+# Record the best parameters. Useful for long parameters.
+def record_best(reward, params):
+    timestr = datetime.datetime.now()
+    f = open('logs/best_{}'.format(model), 'w')
+    f.write("{}|{}".format(repr(params),reward))
+    f.close()
 
-print("observation space: ", env.observation_space)
-print("high: ", env.observation_space.high)
-print("low: ", env.observation_space.low)
-
-input("Press enter to begin")
-
-def run_episode(env, parameters):
-    observation = env.reset()
-    #totalreward = 0
-
-    testmax = -10
-
+def read_best():
+    f = open('./logs/best_{}'.format(model), 'r')
+    fdata = f.read().split("|")
+    return [eval(fdata[0]), float(fdata[1])]
     
-    for _ in range(200):
+# Run one episode and return the reward.
+def run_episode(env, parameters, frames=200):
+    observation = env.reset()
+    for _ in range(frames):
         env.render()
         
-        
-        # action calc
-        action = [0 for i in env.action_space.high]
-        for i in range(len(env.action_space.high)):
+        # Calculate the action based on the weights.
+        action = [0 for i in range(action_size)]
+        for i in range(action_size):
             output = np.matmul(parameters[i], observation)/8
-            #print("output: ", output)
             action[i] = output
-            
-            if np.abs(output) > testmax: testmax = np.abs(output)
-            
-        #print(action)
-
-            
+             
         observation, reward, done, info = env.step(action)
-
         #if done:
         #    print("DONE")
         #    break
-
-    do_render = False
-    #print(testmax)
-    #input()
+        
     return reward
 
-noise_scaling = 0.01
+def get_random_params():
+    return np.random.rand(action_size,observation_size)*2-1
 
-# best worm params of last session
-#3.81196390848482
-#[[-0.01992764 -1.14601906 -0.52076286  0.98520917  0.88103359 -0.82097795
-#   0.58067774 -0.01042194]
-# [-0.26960252 -0.14309877  0.72557209 -0.83686253  1.00713725  0.34495342
-#  -0.68305433 -0.35418443]]
-#
-
-parameters = np.random.rand(action_size,observation_size)*2-1
+new_params = input("New params? y/n ").lower().startswith("y")
 
 
-#bestparams = None
+noise_scaling = 0.1
+
+parameters = get_random_params()
 bestreward = 0
+if not new_params:
+    best = read_best()
+    parameters = best[0]
+    bestreward = best[1]
+
 while True:
-    new_params = parameters + (np.random.rand(action_size,observation_size) * 2 - 1)*noise_scaling
-    reward = np.abs(run_episode(env,parameters))
+    new_params = parameters + get_random_params()*noise_scaling
+    reward = np.abs(run_episode(env,parameters,1000))
     
     if reward > bestreward:
         print(reward)
-        print(new_params)
+        #print(new_params)
+
+        record_best(reward, new_params)
+        
         bestreward = reward
         parameters = new_params
 
